00d94c486eb263d05e0320bc0892b3ab8da687fe
Rick Umali <rickumali@gmail.com>
2017-06-25 19:39:51 -0400

    Removed byline and date from text
    I removed the 'By rickumali Mar. 17, 2017' from the start of every
    article.
    
    Before:
    
        Walking:
        https://www.sportsblog.com/rickumali/world-series-pick-chicago-cubs/
        Found <article>
        Found text By rickumali Oct. 25, 2016I'll be rooting for ...
    
    After:
    
        Walking:
        https://www.sportsblog.com/rickumali/world-series-pick-chicago-cubs/
        Found <article>
        Found text I'll be rooting for ...

--- feedsn.pl
+++ feedsn.pl
@@ -74,6 +74,7 @@ while (my $article_tag = $stream->get_tag("article")) {
 }
 
 my $article_count = $id_counter;
+my $pattern_length = length("By rickumali MMM. DD, YYYY");
 for (my $i = 2; $i <= $article_count; $i++) {
   print "Walking: " . $link{$i} . "\n" if $opt_debug;
   $agent->get($link{$i});
@@ -81,6 +82,7 @@ for (my $i = 2; $i <= $article_count; $i++) {
   while (my $article_tag = $stream->get_tag("article")) {
     print "Found <article>\n" if $opt_debug;
     $text{$i} = get_entry($stream);
+    $text{$i} = substr $text{$i}, $pattern_length;
     print "Found text " . $text{$i} . "\n" if $opt_debug;
   }
 }
